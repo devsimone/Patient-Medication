7b6c98f60ebb6b56e93fc10e0fd9def3eb213654
devsimone <s.roma.lc@gmail.com>
2024-02-12 12:20:18 +0000

    add fixtures for Patient, Clinician, Medication, MedicationRequest to test the future endpoints

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,10 @@ import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.main import app
-from app.config import settings
+from config import settings
 from app.database import get_db
-from app.models import Base
-
+from app.models import Base, Patient, Clinician, Medication, MedicationRequest
+from datetime import date
 
 SQLALCHEMY_DATABASE_URL = (f"{settings.driver_name}://{settings.database_username}:{settings.database_password}@"
                            f"{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test")
@@ -36,3 +36,59 @@ def client(session):
 
     app.dependency_overrides[get_db] = override_get_db
     yield TestClient(app)
+
+
+@pytest.fixture()
+def test_patient(session):
+    patient = Patient(
+        first_name="john",
+        last_name="Patient",
+        date_of_birth=date(1993, 0o6, 13),
+        sex="male",
+    )
+    session.add(patient)
+    session.commit()
+    return patient
+
+
+@pytest.fixture()
+def test_clinician(session):
+    clinician = Clinician(
+        first_name="Laura", last_name="Clinician", registration_id="047383"
+    )
+    session.add(clinician)
+    session.commit()
+    return clinician
+
+
+@pytest.fixture()
+def test_medication(session):
+    medication = Medication(
+        code="123456",
+        code_name="TestMed",
+        code_system="SNOMED",
+        strength_value=5,
+        strength_unit="mg",
+        form="tablet",
+    )
+    session.add(medication)
+    session.commit()
+    return medication
+
+
+@pytest.fixture()
+def medication_request(session, test_patient, test_clinician, test_medication):
+    med_request = MedicationRequest(
+        patient_id=test_patient.id,
+        clinician_id=test_clinician.id,
+        medication_id=test_medication.id,
+        reason_text="Example reason",
+        prescribed_date=date(2019, 12, 4),
+        start_date=date(2019, 12, 4),
+        end_date=date.today(),
+        frequency="Three times a day",
+        status="active",
+    )
+    session.add(med_request)
+    session.commit()
+    return med_request
