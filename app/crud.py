ae20520b079e67e4a9e0953ebafcdb697309d6e2
devsimone <s.roma.lc@gmail.com>
2024-02-12 12:52:59 +0000

    add update medication request operation in crud

--- app/crud.py
+++ app/crud.py
@@ -15,3 +15,20 @@ def get_medication_requests(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.MedicationRequest).offset(skip).limit(limit).all()
 
 
+def update_medication_request(
+    db: Session, request_id: int, request: schemas.MedicationRequestUpdate
+):
+    db_request = (
+        db.query(models.MedicationRequest)
+        .filter(models.MedicationRequest.id == request_id)
+        .first()
+    )
+
+    if db_request:
+        update_data = request.dict(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(db_request, key, value)
+        db.commit()
+        db.refresh(db_request)
+
+    return db_request
